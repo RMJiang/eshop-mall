4bddc8699b3b4d3c9b409ba39b88856a611a6fcc
Ruomeng <920649813@qq.com>
2022-06-02 18:32:08 +0800

    模块整合MyBatisPlus，并给每个服务设置对应的端口

--- mall-coupon/src/main/java/com/eshop/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/eshop/mall/coupon/MallCouponApplication.java
@@ -1,9 +1,11 @@
 package com.eshop.mall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.eshop.mall.coupon.dao")
 public class MallCouponApplication {
 
     public static void main(String[] args) {
--- mall-ware/src/main/java/com/eshop/mall/ware/MallWareApplication.java
+++ mall-ware/src/main/java/com/eshop/mall/ware/MallWareApplication.java
@@ -1,9 +1,11 @@
 package com.eshop.mall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.eshop.mall.ware.dao")
 public class MallWareApplication {
 
     public static void main(String[] args) {
